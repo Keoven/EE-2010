36952df58584fb620bee9e56847d2e8fd40cb64a
Gillian Dugay <gcsdugay@live.com.ph>
2010-05-18 15:23:12 +0800

    Created: Unit test for Candidate model

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,38 @@
 class Candidate < ActiveRecord::Base
+  LEVELS = %w(National Provincial City/Municipal District)
+  POSITIONS = ['President', 'Vice President', 'Senator', 'Governor',
+               'Vice Governor', 'Mayor', 'Vice Mayor', 'Councilor', 'Representative']
+
+  validates_presence_of :first_name, :message => 'required'
+  validates_presence_of :last_name, :message => 'required'
+  validates_inclusion_of :level, :in => LEVELS, :message => 'not in list'
+  validates_inclusion_of :position, :in => POSITIONS, :message => 'not in list'
+  validate :votes_should_not_be_negative
+  validate :level_and_position_should_match
+
+  def self.levels
+    LEVELS
+  end
+
+  def self.positions
+    POSITIONS
+  end
+
+  private
+  def votes_should_not_be_negative
+    errors.add(:num_votes, :message => 'should not be negative') if self.num_votes < 0
+  end
+
+  def level_and_position_should_match
+    case self.level
+      when LEVELS[0]
+        errors.add_to_base('Level and position do not match') unless POSITIONS[0..2].include?(self.position)
+      when LEVELS[1]
+        errors.add_to_base('Level and position do not match') unless POSITIONS[3..4].include?(self.position)
+      when LEVELS[2]
+        errors.add_to_base('Level and position do not match') unless POSITIONS[5..7].include?(self.position)
+      when LEVELS[3]
+        errors.add_to_base('Level and position do not match') unless self.position.eql?(POSITIONS[8])
+    end
+  end
 end
--- test/unit/candidate_test.rb
+++ test/unit/candidate_test.rb
@@ -1,8 +1,72 @@
 require 'test_helper'
 
 class CandidateTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @valid_candidate = candidates(:one)
+  end
+
+  test "check columns" do
+    assert @valid_candidate.first_name.is_a?(String)
+    assert @valid_candidate.middle_name.is_a?(String)
+    assert @valid_candidate.last_name.is_a?(String)
+    assert @valid_candidate.position.is_a?(String)
+    assert @valid_candidate.level.is_a?(String)
+    assert @valid_candidate.province.is_a?(String)
+    assert @valid_candidate.municipality.is_a?(String)
+    assert @valid_candidate.district.is_a?(String)
+    assert @valid_candidate.num_votes.is_a?(Integer)
+  end
+
+  test "should validate presence of first name" do
+    @valid_candidate.first_name = nil
+    @valid_candidate.save
+
+    assert_not_nil @valid_candidate.errors.on(:first_name)
+  end
+
+  test "should validate presence of last name" do
+    @valid_candidate.last_name = nil
+    @valid_candidate.save
+
+    assert_not_nil @valid_candidate.errors.on(:last_name)
+  end
+
+  test "should validate inclusion of position" do
+    @valid_candidate.position = "Standing"
+    @valid_candidate.save
+
+    assert_not_nil @valid_candidate.errors.on(:position)
+  end
+
+  test "should validate inclusion of level" do
+    @valid_candidate.level = "Maximum"
+    @valid_candidate.save
+
+    assert_not_nil @valid_candidate.errors.on(:level)
+  end
+
+  test "should validate non-negative votes" do
+    @valid_candidate.num_votes = -88
+    @valid_candidate.save
+
+    assert_not_nil @valid_candidate.errors.on(:num_votes)
+  end
+
+  test "level and position should match" do
+    @valid_candidate.level = 'National'
+    @valid_candidate.position = 'Representative'
+    @valid_candidate.save
+
+    assert_not_nil @valid_candidate.errors
+  end
+
+  test "location should match level" do
+    @valid_candidate.level = 'National'
+    @valid_candidate.province = 'Provincia'
+    @valid_candidate.municipality = 'Ciudad'
+    @valid_candidate.district = 'Distrito'
+    @valid_candidate.save
+
+    assert_not_nil @valid_candidate.errors
   end
 end
