db1c5f88c38c60cfe108ded2416cf4044ed5fb4d
Nelvin Driz <NelvinDriz@live.com>
2010-05-19 15:22:38 +0800

    Fixed: Conflict in seed.rb; Added: admins_routing_test.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+require 'active_record/fixtures'
 
 Admin.create(:id                    => 1                   ,
              :login                 => 'admin'             ,
@@ -13,8 +6,5 @@ Admin.create(:id                    => 1                   ,
              :password              => 'minda'             ,
              :password_confirmation => 'minda'             )
 
-=======
-require 'active_record/fixtures'
-
 Fixtures.create_fixtures("#{Rails.root}/db/migrate/data","candidates")
->>>>>>> d99fd58e1349664ec7f67b56691963a719911478
+
