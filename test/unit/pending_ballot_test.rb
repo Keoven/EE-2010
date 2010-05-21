9ffd039733b31950794e60720d6234f173df4575
Nelvin Driz <NelvinDriz@live.com>
2010-05-21 11:45:34 +0800

    Fixed: PendingBallot unit test

--- test/unit/pending_ballot_test.rb
+++ test/unit/pending_ballot_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PendingBallotTest < ActiveSupport::TestCase
   test 'ballot should belong to a voter' do
-    assert_response_to :voter
+    assert(pending_ballots(:one).respond_to? :voter)
   end
 end
 
