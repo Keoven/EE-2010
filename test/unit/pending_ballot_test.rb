a84ed4af7adfee735a25f38eee6c16ff55ccbc33
Nelvin Driz <NelvinDriz@live.com>
2010-05-21 11:41:13 +0800

    Updated: Pending Ballot Models and Test

--- test/unit/pending_ballot_test.rb
+++ test/unit/pending_ballot_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PendingBallotTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test 'ballot should belong to a voter' do
+    assert_response_to :voter
   end
 end
+
