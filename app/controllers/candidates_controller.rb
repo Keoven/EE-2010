557a7a8d5b30415f1b560ac9ee8008086dcbefd0
Gillian Dugay <gcsdugay@live.com.ph>
2010-05-25 02:45:55 +0800

    Fixed: view for partial tally

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -34,24 +34,104 @@ class CandidatesController < ApplicationController
                                     :order    => 'num_votes DESC'
   end
   
-  def list_municipalities
+  def update_tally
+    render :update do |page|
+      Candidate.positions[0..2].each do |position|
+        position = position.gsub(' ','').underscore
+        page.replace "#{position}_tally".intern,
+          :partial => 'tally_position',
+          :locals  => { :divsion  => 'national',
+                        :position => position }
+      end
+      
+      Candidate.positions[3..8].each do |position|
+        position = position.gsub(' ','').underscore
+        page.replace "#{position}_tally".intern,
+          :partial => 'tally_position',
+          :locals  => { :division => 'local',
+                        :position => position }
+      end
+    end
+  end
+  
+  def choose_province
     municipality_list = MUNICIPALITY_LIST[params[:province]].sort
 
     render :update do |page|
       page.replace_html :municipality_tally_filter, :partial => 'municipality_list', :locals => { :list => municipality_list }
       page.replace_html :district_tally_filter, :partial => 'district_list',
         :locals => { :list => generate_district_list(params[:province], municipality_list.first[1]) }
-      page.replace_html :local_candidate_records, :partial => 'tally_position',
-        :locals => { :prefix => 'local',
-                     :list => [Candidate.for_governor.find_by_province( PROVINCE_LIST.index(params[:province]) )] }
+      Candidate.positions[3..4].each do |position|
+        position = position.gsub(' ','').underscore
+        page.replace "#{position}_tally".intern,
+          :partial => 'tally_position',
+          :locals  => { :division => 'local',
+                        :position => position,
+                        :province => PROVINCE_LIST.index(params[:province]) }
+      end
+      Candidate.positions[5..7].each do |position|
+        position = position.gsub(' ','').underscore
+        page.replace "#{position}_tally".intern,
+          :partial => 'tally_position',
+          :locals  => { :division => 'local',
+                        :position => position,
+                        :province => PROVINCE_LIST.index(params[:province]),
+                        :municipality => MUNICIPALITY_LIST[params[:province]].sort.first[0] }
+      end
+      Candidate.positions[8].each do |position|
+        position = position.gsub(' ','').underscore
+        page.replace "#{position}_tally".intern,
+          :partial => 'tally_position',
+          :locals  => { :division => 'local',
+                        :position => position,
+                        :province => PROVINCE_LIST.index(params[:province]),
+                        :municipality => MUNICIPALITY_LIST[params[:province]].sort.first[0],
+                        :district => 1 }
+      end
     end
   end
 
-  def list_districts
+  def choose_municipality
     district_list = generate_district_list(params[:province], params[:municipality])
 
     render :update do |page|
+      Candidate.positions[5..7].each do |position|
+        position = position.gsub(' ','').underscore
+        page.replace "#{position}_tally".intern,
+          :partial => 'tally_position',
+          :locals  => { :division => 'local',
+                        :position => position,
+                        :province => PROVINCE_LIST.index(params[:province]),
+                        :municipality => MUNICIPALITY_LIST[params[:province]].index(params[:municipality]) }
+      end
+      
       page.replace_html :district_tally_filter, :partial => 'district_list', :locals => { :list => district_list }
+      
+      Candidate.positions[8].each do |position|
+        position = position.gsub(' ','').underscore
+        page.replace "#{position}_tally".intern,
+          :partial => 'tally_position',
+          :locals  => { :division => 'local',
+                        :position => position,
+                        :province => PROVINCE_LIST.index(params[:province]),
+                        :municipality => MUNICIPALITY_LIST[params[:province]].index(params[:municipality]),
+                        :district => 1 }
+      end
+    end
+  end
+  
+  def choose_district
+    render :update do |page|
+      Candidate.positions[8].each do |position|
+        position = position.gsub(' ','').underscore
+        page.replace "#{position}_tally".intern,
+          :partial => 'tally_position',
+          :locals  => { :division => 'local',
+                        :position => position,
+                        :province => PROVINCE_LIST.index(params[:province]),
+                        :municipality => MUNICIPALITY_LIST[params[:province]].index(params[:municipality]),
+                        :district => params[:district].last.to_i }
+      end
     end
   end
 
